797f8a5af33ffd627cf24165a4c96bf4b9bd9829
forteleaf <forteleaf@hotmail.com>
2024-10-12 21:57:27 +0900

    complete reverse-string

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,13 +1,8 @@
 pub fn reverse(input: &str) -> String {
-     todo!("Write a function to reverse {input}");
-
     let input_input = input;
     let mut output = String::new();
     for c in input_input.chars().rev() {
         output.push(c);
     }
     output
-
-
-
 }
