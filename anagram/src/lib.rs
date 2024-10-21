ddca7ff963fd2c1212fec625612f6e7d6c7052e9
forteleaf <forteleaf@hotmail.com>
2024-10-22 08:20:43 +0900

    complate anagram (7)

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,17 +2,23 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     //todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
-    let word_hash = word.chars().collect::<Vec<_>>();
-    let mut result: HashSet<&str> = HashSet::new();
+    let word_lower = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lower);
 
-    // 각 단어가의-character-리스트를 해시 테이블에 넣어두고,
-    for anagram in possible_anagrams {
-        let anagram_hash = anagram.chars().collect::<Vec<_>>();
+    possible_anagrams
+        .iter()
+        .filter(|&x| {
+            let x_lower = x.to_lowercase();
+            x.len() == word_lower.len() // 길이가 같고
+                && x_lower != word_lower // 같은 단어가 아니고,
+                && get_sorted(&x_lower) == word_sorted // 정렬했을 때, 같은 단어가 아님.
+        })
+        .copied()
+        .collect()
+}
 
-        if anagram_hash == word_hash {
-            result.insert(anagram);
-        }
-    }
-
-    result
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted = word.chars().collect::<Vec<char>>();
+    word_sorted.sort_unstable();
+    word_sorted.into_iter().collect()
 }
