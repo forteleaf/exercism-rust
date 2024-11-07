2b3bef67cf76244acab63b057a516cc02629c7e7
forteleaf <forteleaf@hotmail.com>
2024-11-07 23:02:27 +0900

    complete leap (1/5) #2

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -2,13 +2,5 @@ use std::ops::Rem;
 
 pub fn is_leap_year(year: u64) -> bool {
     // todo!("true if {year} is a leap year")
-    if year.rem(4) == 0 {
-        if year.rem(400) == 0 {
-            true
-        } else {
-            year.rem(100) != 0
-        }
-    } else {
-        false
-    }
+    year.rem(4) == 0 && (year.rem(100) != 0 || year.rem(400) == 0)
 }
