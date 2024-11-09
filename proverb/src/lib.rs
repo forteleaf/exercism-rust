2099c112d888ec8f76448671403783e5b718c010
forteleaf <forteleaf@hotmail.com>
2024-11-09 22:52:21 +0900

    complete proverb (1/5) #2
    다른사람이 한 작업내용

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,16 +1,12 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    // todo!("build a proverb from this list of items: {list:?}")
-    let mut aa: Vec<String> = vec![];
-    for i in 0..list.len() {
-        if i == list.len() - 1 {
-            aa.push(format!("And all for the want of a {}.", list[0]))
-        } else {
-            aa.push(format!(
-                "For want of a {} the {} was lost.",
-                list[i],
-                list[i + 1]
-            ))
-        }
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
-    aa.join("\n")
 }
