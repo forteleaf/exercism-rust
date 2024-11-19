cdb4bf9071e5470684430394555a4b7cea18f844
forteleaf <forteleaf@hotmail.com>
2024-11-19 22:48:07 +0900

    complete sum-of-multiple #2
    간추린 답안

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,16 +1,5 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    // todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
-    let mut set: HashSet<u32> = HashSet::new();
-    factors.iter().for_each(|x| {
-        for i in 0..limit {
-            if x * i < limit {
-                set.insert(x * i);
-            } else {
-                break;
-            }
-        }
-    });
-    set.iter().sum()
+    (1..limit)
+        .filter(|x| factors.iter().any(|d| *d != 0 && x % d == 0))
+        .sum()
 }
