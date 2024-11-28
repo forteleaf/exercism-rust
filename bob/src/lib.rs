b4130f041820d21136b601478999afb597110f30
forteleaf <forteleaf@hotmail.com>
2024-11-28 22:16:18 +0900

    complete bob #2 next

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,14 +1,10 @@
 pub fn reply(message: &str) -> &str {
     // todo!("have Bob reply to the incoming message: {message}")
-    if message.trim().is_empty() {
-        "Fine. Be that way!"
-    } else if message == "WHAT'S GOING ON?" {
-        "Calm down, I know what I'm doing!"
-    } else if message == message.to_uppercase() && message.chars().any(|f| f.is_alphabetic()) {
-        "Whoa, chill out!"
-    } else if message.trim().ends_with('?') {
-        "Sure."
-    } else {
-        "Whatever."
+    match message.trim() {
+        m if m.is_empty() => "Fine. Be that way!",
+        m if m == "WHAT'S GOING ON?" => "Calm down, I know what I'm doing!",
+        m if m == m.to_uppercase() && m.chars().any(|f| f.is_alphabetic()) => "Whoa, chill out!",
+        m if m.trim().ends_with('?') => "Sure.",
+        _ => "Whatever.",
     }
 }
