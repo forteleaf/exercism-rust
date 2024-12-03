34af2afab065f854c16a45e41e49599010976f2d
forteleaf <forteleaf@hotmail.com>
2024-12-03 22:49:17 +0900

    complete matching-bracket #2

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,28 +1,29 @@
+use std::collections::VecDeque;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    // todo!("Check if the string \"{string}\" contains balanced brackets");
-    let mut stack = Vec::new();
+    let mut stack = VecDeque::new();
 
     // 문자열의 각 문자를 순회
     for c in string.chars() {
         match c {
             // 여는 괄호인 경우 스택에 푸시
-            '{' | '[' | '(' => stack.push(c),
+            '{' | '[' | '(' => stack.push_back(c),
             // 닫는 중괄호인 경우 스택의 마지막 값이 여는 중괄호인지 확인
             '}' => {
-                if stack.pop() != Some('{') {
+                if stack.pop_back() != Some('{') {
                     return false;
                 }
             }
 
             // 닫는 대괄호인 경우 스택의 마지막 값이 여는 대괄호인지 확인
             ']' => {
-                if stack.pop() != Some('[') {
+                if stack.pop_back() != Some('[') {
                     return false;
                 }
             }
             // 닫는 소괄호인 경우 스택의 마지막 값이 여는 소괄호인지 확인
             ')' => {
-                if stack.pop() != Some('(') {
+                if stack.pop_back() != Some('(') {
                     return false;
                 }
             }
